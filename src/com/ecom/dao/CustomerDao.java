661b755a29a9c1ea625ce5aedc5a41082f7ec934
Arthic17 <133387385+Arthic17@users.noreply.github.com>
2025-06-04 17:58:26 +0530

    Delete src/com directory

--- src/com/ecom/dao/CustomerDao.java
+++
@@ -1,8 +0,0 @@
-package com.ecom.dao;
-
-import com.ecom.exception.InvalidIdException;
-import com.ecom.model.*;
-
-public interface CustomerDao {
-	Customer getById(int id) throws InvalidIdException;
-}
